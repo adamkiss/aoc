06dc4810a044d9f6f056f0aa4fbc88d9acbbeeba
Adam Kiss <iam@adamkiss.com>
2024-12-15 19:54:40 +0100

    2024 / php / 15 alt v (fail)

--- php/2024/15-ds.php
+++ php/2024/15-ds.php
@@ -1,11 +1,12 @@
 <?php
 
+use Ds\Map;
 use Ds\Set;
 use Kirby\Toolkit\A;
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$input = read_input();
+$input = read_input('15.txt');
 $input_demo = <<<INPUT
 ##########
 #..O..O.O#
@@ -66,6 +67,20 @@ function print_map(array $map, ?int $x = null, ?int $y = null) {
 	println();
 }
 
+function print_ds_map(Map $m, ?int $x = null, ?int $y = null) {
+	$ll = -1;
+	foreach ($m as $k => $v) {
+		if ($k[1] > $ll) {
+			println();
+			$ll = $k[1];
+		}
+		if ($x && $y && $k === [$x,$y]) {
+			print 'X';
+		} else {
+			print $v;
+		}
+	}
+}
 
 function process_input(string $input): array {
 	[$map_raw, $moves_raw] = explode("\n\n", $input);
@@ -79,16 +94,29 @@ function process_input(string $input): array {
 
 function process_input2(string $input): array {
 	[$map_raw, $moves_raw] = explode("\n\n", $input);
-	$map = array_map(
-		array: explode("\n", $map_raw),
-		callback: fn($line) => str_split(str_replace(
+	$map = new Map();
+
+	$w = 1;
+	$h = 1;
+	$bx = -1;
+	$by = -1;
+	foreach (explode("\n", $map_raw) as $y => $line) {
+		$h++;
+		foreach (str_split(str_replace(
 			['#', 'O', '.', '@'],
 			['##', '[]', '..', '@.'],
 			$line
-		))
-	);
+		)) as $x => $ch) {
+			$w++;
+			$map[[$x,$y]] = $ch;
+			if ($ch === '@') {
+				$bx = $x;
+				$by = $y;
+			}
+		}
+	}
 	$moves = str_split(str_replace("\n", '', $moves_raw));
-	return [$map, $moves];
+	return [$map, $moves, $w, $h, $bx, $by];
 }
 
 function vec_add(int $x1, int $y1, int $x2, int $y2): array {
@@ -153,121 +181,111 @@ function part1 (string $input) {
 	return $sum;
 }
 
-function p2moveh(array &$map, int $x, int $y, $dir, int $i) : bool {
+function p2moveh(Map &$m, int $x, int $y, $dir, int $i) : bool {
 	[$nx, $ny] = vec_add($x, $y, $dir[0], $dir[1]);
 
-	if ($map[$ny][$nx] === '#') {
+	if ($m[[$nx,$ny]] === '#') {
 		return false;
 	}
-	$canmove = ($map[$ny][$nx] === ']' || $map[$ny][$nx] === '[')
-		? p2moveh($map, $nx, $ny, $dir, $i+1)
+	$canmove = ($m[[$nx,$ny]] === ']' || $m[[$nx,$ny]] === '[')
+		? p2moveh($m, $nx, $ny, $dir, $i+1)
 		: true;
 
 	if (! $canmove) {
 		return false;
 	}
 
-	$map[$ny][$nx] = $map[$y][$x];
+	$m[[$nx,$ny]] = $m[[$x,$y]];
 	return true;
 }
 
-function p2checkv(array $map, int $x, int $y, $dir, int $i) : array|bool {
-	[$nx, $ny] = vec_add($x, $y, $dir[0], $dir[1]);
-	if ($map[$ny][$nx] === '#') {
+function p2checkv(Map $m, int $x, int $y, $d, int $i) : Set|bool {
+	$ch = $m[[$x,$y]];
+
+	if ($ch === '#') {
 		return false;
 	}
 
-	if ($map[$ny][$nx] === '.') {
+	if ($ch === '.') {
 		return true;
 	}
 
-	$fhalf = p2checkv($map, $nx, $ny, $dir, $i + 1);
+	$fhalf = p2checkv($m, $x, $y + $d[1], $d, $i + 1);
 	if ($fhalf === false) {
 		return false;
 	}
 
-	$shalf = ($map[$ny][$nx] === ']')
-		? p2checkv($map, $nx-1, $ny, $dir, $i + 1)
-		: p2checkv($map, $nx+1, $ny, $dir, $i + 1);
+	$shalf = ($ch === ']')
+		? p2checkv($m, $x-1, $y + $d[1], $d, $i + 1)
+		: p2checkv($m, $x+1, $y + $d[1], $d, $i + 1);
 
 	if ($fhalf === false || $shalf === false) {
 		return false;
 	}
 
-	$self = $map[$ny][$nx] === ']'
-		? [[$nx-1, $ny]]
-		: [[$nx, $ny]];
+	$self = $m[[$x,$y]] === ']'
+		? new Set([[$x-1, $y]])
+		: new Set([[$x, $y]]);
 
-	if ($shalf === true && $fhalf === true) {
-		return $self;
-	}
+	ray([$self, $shalf, $fhalf]);
 
-	return array_merge(is_array($fhalf) ? $fhalf : [], is_array($shalf) ? $shalf : [], $self);
+	$self = ($fhalf !== true) ? $self->union($fhalf) : $self;
+	$self = ($shalf !== true) ? $self->union($shalf) : $self;
+
+	return $self;
 }
 
 function part2 (string $input) {
-	[$map, $moves] = process_input2($input);
-
-	$h = count($map);
-	$w = count($map[0]);
-
-	$bx = -1;
-	$by = -1;
-	for ($i=0; $i < $h; $i++) {
-		for ($j=0; $j < $w; $j++) {
-			if ($map[$i][$j] !== '@') {
-				continue;
-			}
-			$bx = $j;
-			$by = $i;
-			break 2;
-		}
-	}
+	[$map, $moves, $w, $h, $bx, $by] = process_input2($input);
 
 	foreach ($moves as $move) {
 		$d = DIRS[$move];
 
+		// println($move, ...$d);
+		// print_ds_map($map);
+		// readline();
+
 		if ($move === '<' || $move === '>') {
 			$moved = p2moveh($map, $bx, $by, $d, 0);
 			if ($moved) {
-				$map[$by][$bx] = '.';
+				$map[[$bx,$by]] = '.';
 				[$bx, $by] = vec_add($bx, $by, $d[0], $d[1]);
 			}
 			continue;
 		}
 
-		$canmove = p2checkv($map, $bx, $by, $d, 0);
+		[$nx, $ny] = vec_add($bx, $by, $d[0], $d[1]);
+		$nextmove = p2checkv($map, $nx, $ny, $d, 0);
 
-		if ($canmove === false) {
+		if ($nextmove === false) {
 			continue;
 		}
 
-		if (is_array($canmove)) {
-			$unique = (new Set($canmove))->toArray();
-			foreach ($unique as [$boxx, $boxy]) {
-				[$movx, $movy] = vec_add($boxx, $boxy, $d[0], $d[1]);
-				$map[$movy][$movx+0] = '[';
-				$map[$movy][$movx+1] = ']';
-				$map[$boxy][$boxx+0] = '.';
-				$map[$boxy][$boxx+1] = '.';
+		if ($nextmove !== true) {
+			$nextmove->sort(fn ($a, $b) => $d[1] === -1 ? $a <=> $b : $b <=> $a);
+			foreach ($nextmove as [$boxx, $boxy]) {
+				[$_, $movy] = vec_add($boxx, $boxy, $d[0], $d[1]);
+				$map[[$boxx+0, $movy]] = '[';
+				$map[[$boxx+1, $movy]] = ']';
+				$map[[$boxx+0, $boxy]] = '.';
+				$map[[$boxx+1, $boxy]] = '.';
 			}
 		}
 
-		$map[$by][$bx] = '.';
-		[$bx, $by] = vec_add($bx, $by, $d[0], $d[1]);
-		$map[$by][$bx] = '@';
+		$map[[$bx,$by]] = '.';
+		$by = $ny;
+		$map[[$bx,$by]] = '@';
 	}
 
-	print_map($map);
+	// var_dump($map);
+	print_ds_map($map);
 
 	$sum = 0;
-	for ($i=0; $i < $h; $i++) {
-		for ($j=0; $j < $w; $j++) {
-			if ($map[$i][$j] !== '[') {
-				continue;
-			}
-			$sum += $i * 100 + $j;
+	foreach ($map as $k => $v) {
+		if ($v !== '[') {
+			continue;
 		}
+		$sum += $k[1] * 100 + $k[0];
 	}
 
 	return $sum;
@@ -298,8 +316,8 @@ println('2) Result of demo: ' . $r);
 printf("» %.3fms\n", (microtime(true)-$p) * 1000);
 assert($r === 9021);
 
-// $p = microtime(true);
-// println('2) Result of real input: ' . part2($input));
-// printf("» %.3fms\n", (microtime(true)-$p) * 1000);
+$p = microtime(true);
+println('2) Result of real input: ' . part2($input));
+printf("» %.3fms\n", (microtime(true)-$p) * 1000);
 
 printf("TOTAL: %.3fms\n", (microtime(true)-$s) * 1000);
