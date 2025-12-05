9151fc6c380c7d109b922f45b1f51617174d1b12
Adam Kiss <iam@adamkiss.com>
2025-12-05 13:31:42 +0100

    go / 2025 / prepare day 05

---
+++ go/2025/05/day05.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/adamkiss/aoc/go/utils"
+)
+
+var input string = utils.ReadInput("05")
+var inputdemo string = `
+ABC
+`
+
+func Part1(i string) int {
+	return 1
+}
+
+func Part2(i string) int {
+	return 2
+}
+
+func main() {
+	start := time.Now()
+
+	var r1 int
+	demo1expected := 1
+	r1 = Part1(inputdemo)
+	if r1 != demo1expected {
+		panic(fmt.Sprintf("Part 1 demo failed: %d, expected %d", r1, demo1expected))
+	}
+	r1 = Part1(input)
+	fmt.Printf("Part 1: %d\n", r1)
+
+	p01time := time.Since(start)
+
+	//
+	// Part 02
+	//
+	start2 := time.Now()
+
+	// var r2 int
+	// demo2expected := 2
+	// r2 = Part2(inputdemo)
+	// if r2 != demo2expected {
+	// 	panic(fmt.Sprintf("Part 2 demo failed: %d, expected %d", r2, demo2expected))
+	// }
+	// r2 = Part2(input)
+	// fmt.Printf("Part 2: %d\n", r2)
+
+	p02time := time.Since(start2)
+
+	//
+	// Output
+	//
+	fmt.Println()
+	fmt.Println("Runtimes ↴")
+	fmt.Printf("Day 05 Part 1: %s\n", p01time)
+	fmt.Printf("Day 05 Part 2: %s\n", p02time)
+	fmt.Printf("Day 05 Total : %s\n", time.Since(start))
+}
